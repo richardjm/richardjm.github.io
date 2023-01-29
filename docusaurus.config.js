161c60144fa9930f654702f18c5048506201e006
Richard Mitchell <2081919+richardjm@users.noreply.github.com>
2023-01-29 17:15:54 +0000

    richardjm

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Richard J M',
+  title: 'richardjm',
   tagline: 'General dogsbody',
   favicon: 'img/logo.svg',
 
