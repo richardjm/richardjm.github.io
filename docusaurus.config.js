003b9dd68850fbd57d62c0e9e1fc13da29e78f94
Richard Mitchell <2081919+richardjm@users.noreply.github.com>
2025-11-22 21:15:54 +0000

    Latest npm packages
    Remove deprecated option and use sensible warn default

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,6 @@ const config = {
   trailingSlash: false,
 
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
