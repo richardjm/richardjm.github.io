32c478b679727a6e1b1a72363d90eaf3557bd5d2
Richard Mitchell <2081919+richardjm@users.noreply.github.com>
2026-03-21 09:37:35 +0000

    Try github reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,12 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  reporter: [
+    ['list'],
+    ['github'],
+    ['html', { open: 'never' }],
+    ['junit', { outputFile: 'test-results/junit.xml' }],
+  ],
   use: {
     baseURL: 'http://localhost:3000',
     trace: 'on-first-retry',
